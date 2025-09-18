09f8047b928a74513384c380fb2eb9535727ce3f
Poutchy <enzo.mauti@laposte.net>
2025-09-18 10:00:39 +0200

    tests modification

--- src/process.py
+++ src/process.py
@@ -11,9 +11,9 @@ class Process(Thread):
 
         self.com = Com()
 
-        self.nbProcess = self.com.getNbProcess()
+        self.nbProcess = self.com.get_nb_process()
 
-        self.myId = self.com.getMyId()
+        self.myId = self.com.get_my_id()
         self.name = name
 
         self.alive = True
@@ -26,13 +26,12 @@ class Process(Thread):
             sleep(1)
 
             if self.name == "P0":
-                # self.com.sendTo("j'appelle 2 et je te recontacte après", 1)
+                self.com.send_to("j'appelle 2 et je te recontacte après", 1)
 
                 # self.com.sendToSync(
                 #   "J'ai laissé un message à 2, je le rappellerai après, on se sychronise tous et on attaque la partie ?",
                 #   2,
                 # )
-                # msg = [""]
                 # self.com.recevFromSync(msg, 2)
 
                 # self.com.sendToSync(
@@ -41,51 +40,53 @@ class Process(Thread):
 
                 # self.com.synchronize()
 
-                self.com.requestSC()
-                # if self.com.mailbox.isEmpty():
+                # self.com.request_s_c()
+                # if self.com.mailbox.is_empty():
                 #     print("Catched !")
                 #     self.com.broadcast("J'ai gagné !!!")
                 # else:
-                #     msg = self.com.mailbox.getMsg()
-                #     print(str(msg.getSender()) + " à eu le jeton en premier")
-                self.com.releaseSC()
+                #     msg = self.com.mailbox.get_msg()
+                #     print(str(msg.get_sender()) + " à eu le jeton en premier")
+                # self.com.release_s_c()
 
             if self.name == "P1":
-                if not self.com.mailbox.isEmpty():
-                    # self.com.mailbox.getMsg()
-                    # msg = [""]
+                if not self.com.mailbox.is_empty():
+                    msg = self.com.mailbox.get_msg()
+                    print(f"{self.name} received {msg}")
                     # self.com.recevFromSync(msg, 0)
 
                     # self.com.synchronize()
 
-                    self.com.requestSC()
-                    # if self.com.mailbox.isEmpty():
-                    #   print("Catched !")
-                    #   self.com.broadcast("J'ai gagné !!!")
-                    # else:
-                    #   msg = self.com.mailbox.getMsg()
-                    #   print(str(msg.getSender()) + " à eu le jeton en premier")
-                    self.com.releaseSC()
+                #     self.com.request_s_c()
+                #     if self.com.mailbox.is_empty():
+                #         print("Catched !")
+                #         self.com.broadcast("J'ai gagné !!!")
+                #     else:
+                #         msg = self.com.mailbox.get_msg()
+                #         print(str(msg.get_sender()) + " à eu le jeton en premier")
+                #     self.com.release_s_c()
 
-            if self.name == "P2":
-                # msg = [""]
-                # self.com.recevFromSync(msg, 0)
-                # self.com.sendToSync("OK", 0)
+            # if self.name == "P2":
+            # msg = [""]
+            # self.com.recevFromSync(msg, 0)
+            # self.com.sendToSync("OK", 0)
 
-                # self.com.synchronize()
+            # self.com.synchronize()
 
-                self.com.requestSC()
-                # if self.com.mailbox.isEmpty():
-                #   print("Catched !")
-                #   self.com.broadcast("J'ai gagné !!!")
-                # else:
-                #   msg = self.com.mailbox.getMsg()
-                #   print(str(msg.getSender()) + " à eu le jeton en premier")
-                self.com.releaseSC()
+            # self.com.request_s_c()
+            # if self.com.mailbox.is_empty():
+            #     print("Catched !")
+            #     self.com.broadcast("J'ai gagné !!!")
+            # else:
+            #     msg = self.com.mailbox.get_msg()
+            #     print(str(msg.get_sender()) + " à eu le jeton en premier")
+            # self.com.release_s_c()
 
             loop += 1
         print(self.name + " stopped")
 
     def stop(self):
         self.alive = False
+
+    def waitStopped(self):
         self.join()
