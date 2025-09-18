ab240b61d184318d5eb1141928f701cced26f234
Poutchy <enzo.mauti@laposte.net>
2025-09-18 10:00:18 +0200

    mailbox modification

--- src/com/mailbox/mailbox.py
+++ src/com/mailbox/mailbox.py
@@ -1,4 +1,5 @@
 from collections import deque
+from typing import Optional
 
 from com.messages.communication import CommunicationMessage
 
@@ -10,11 +11,19 @@ class MailBox:
         super().__init__()
         self.msgs: deque[CommunicationMessage] = deque()
 
-    def isEmpty(self) -> bool:
-        return len(self.msgs) > 0
+    def is_empty(self) -> bool:
+        return len(self.msgs) == 0
 
-    def getMsg(self) -> CommunicationMessage:
+    def get_msg(self) -> CommunicationMessage:
+        """Function that answer with the first message of the MailBox
+
+        raise: IndexError
+        return: The first message to arrive if there are some, None if not
+        :rtype: CommunicationMessage
+        """
+        if self.is_empty():
+            raise IndexError()
         return self.msgs.popleft()
 
-    def addMsg(self, message: CommunicationMessage):
+    def add_msg(self, message: CommunicationMessage):
         return self.msgs.append(message)
